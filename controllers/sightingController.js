3a70241622a7874b442d2307efd582376b271539
Joseph Trubenstein <josephtrubenstein@gmail.com>
2019-06-05 12:35:35 -0400

    third

--- controllers/sightingController.js
+++ controllers/sightingController.js
@@ -5,19 +5,88 @@ module.exports = {
     return new Promise((resolve, reject) => {
       const newSighting = new Sighting();
 
-      newSighting.witness = params.witness,
-      newSighting.seenDate = params.seenDate,
-      newSighting.location = params.location,
-      newSighting.description = params.description
+      (newSighting.witness = params.witness),
+        (newSighting.seenDate = params.seenDate),
+        (newSighting.location = params.location),
+        (newSighting.description = params.description);
 
-      newSighting.save()
-      .then( sighting => {
-          resolve(sighting)
-      })
-      .catch(error => {
-          console.log(error)
-          reject(error)
-      })
+      newSighting
+        .save()
+        .then(sighting => {
+          resolve(sighting);
+        })
+        .catch(error => {
+          console.log(error);
+          reject(error);
+        });
+    });
+  },
+
+  approveSighting: function(req) {
+    return new Promise((resolve, reject) => {
+      Sighting.findByIdAndUpdate(req.params.id)
+        .then(sighting => {
+          let approvedSighting = sighting;
+          approvedSighting.isApproved = true;
+          approvedSighting
+            .save()
+            .then(savedSighting => {
+              resolve(savedSighting);
+            })
+            .catch(error => {
+              reject(error);
+            });
+          resolve(approvedSighting);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+
+  rejectSighting: function(req) {
+    return new Promise((resolve, reject) => {
+      Sighting.findByIdAndUpdate(req.params.id)
+        .then(sighting => {
+          let rejectedSighting = sighting;
+          rejectedSighting.isApproved = false;
+          rejectedSighting
+            .save()
+            .then(savedSighting => {
+              resolve(savedSighting);
+            })
+            .catch(error => {
+              reject(error);
+            });
+          resolve(rejectedSighting);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+
+  getAllSightings: function() {
+    return new Promise((resolve, reject) => {
+      Sighting.find({})
+        .then(sightings => {
+          resolve(sightings);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+
+  reviewSighting: function(req) {
+    return new Promise((resolve, reject) => {
+      Sighting.findById(req.params.id)
+        .then(sighting => {
+          resolve(sighting);
+        })
+        .catch(error => {
+          reject(error);
+        });
     });
   }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-var passport = require('passport')
+var passport = require("passport");
 
 var sightingController = require("../controllers/sightingController");
 
@@ -11,35 +11,92 @@ router.get("/", function(req, res, next) {
   res.send("respond with a resource");
 });
 
+/* GET all Sightings */
+router.get("/admin-dashboard", function(req, res, next) {
+  sightingController
+    .getAllSightings()
+    .then(sightings => {
+      res.render("dash", {
+        title: "Welcome Mothman Admin",
+        sightings: sightings
+      });
+    })
+    .catch(error => {
+      res.json(error);
+    });
+});
+
+/* GET a Sighting by ID for review */
+router.get("/admin-dashboard/:id", function(req, res, next) {
+  sightingController
+    .reviewSighting(req)
+    .then(sighting => {
+      res.render("review", {
+        title: "Welcome Mothman Admin",
+        sighting: sighting
+      });
+    })
+    .catch(error => {
+      res.json(error);
+    });
+});
+
 // GET in via passport with username and password
-router.post('/signin', passport.authenticate('local-login', {
-  successRedirect: '/',
-  failureRedirect: '/users/signin',
-  failureFlash: true
-}))
+router.post(
+  "/signin",
+  passport.authenticate("local-login", {
+    successRedirect: "/",
+    failureRedirect: "/users/signin",
+    failureFlash: true
+  })
+);
 
 /* POST a new user. */
 router.post("/signup", function(req, res, next) {
-  userController.signUp(req.body)
-  .then( user => {
-    res.json(user)
-  })
-  .catch( error => {
-    res.json(error)
-  })
+  userController
+    .signUp(req.body)
+    .then(user => {
+      res.json(user);
+    })
+    .catch(error => {
+      res.json(error);
+    });
 });
 
 /* POST a Sighting */
-router.post(
-  "/new-sighting", function(req, res, next) {
-    sightingController.newSighting(req.body)
-    .then( sighting => {
-        res.json(sighting)
+router.post("/new-sighting", function(req, res, next) {
+  sightingController
+    .newSighting(req.body)
+    .then(sighting => {
+      res.json(sighting);
     })
     .catch(error => {
-      res.json(error)
-    })
-   } );
+      res.json(error);
+    });
+});
 
+/* GET a Sighting Approval */
+router.get("/admin-dashboard/approve-sighting/:id", function(req, res, next) {
+  sightingController
+    .approveSighting(req)
+    .then(sighting => {
+      res.redirect("../../admin-dashboard");
+    })
+    .catch(error => {
+      res.json(error);
+    });
+});
+
+/* GET a Sighting Rejection */
+router.get("/admin-dashboard/reject-sighting/:id", function(req, res, next) {
+  sightingController
+    .rejectSighting(req)
+    .then(sighting => {
+      res.redirect("../../admin-dashboard");
+    })
+    .catch(error => {
+      res.json(error);
+    });
+});
 
 module.exports = router;
