69b4007ef63325c95d7f509168f543ebb796ebcd
Joseph Trubenstein <josephtrubenstein@gmail.com>
2019-06-10 11:25:15 -0400

    new schema

--- models/Sighting.js
+++ models/Sighting.js
@@ -4,11 +4,11 @@ const moment = require('moment');
 let SightingSchema = new mongoose.Schema({
     witness: { type: String, default: '' },
     seenDate: { type: String, default: '' },
-    location: { type: String, default: '' },
+    location: { type: Object, default: {} },
     description: { type: String, default: '' },
     imageUrl: { type: String, default: '' },
     isApproved: { type: Boolean, default: false },
     submitDate: { type: String, default: () => moment().format('dddd, MMMM Do YYYY, h:mm:ss a')},
-})
+ })
 
 module.exports = mongoose.model('sighting', SightingSchema);
